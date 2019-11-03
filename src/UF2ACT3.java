c3d40a71e64e5c241e59969aec86ecd7239ec0bd
jaade <jaade@DESKTOP-4S5CDDG>
2019-11-03 14:31:25 +0100

    segon

--- src/UF2ACT3.java
+++ src/UF2ACT3.java
@@ -9,7 +9,7 @@ public class UF2ACT3 extends RecursiveTask<Long> {
     } 
     @Override
     protected Long compute() {
-        // ATENCIO **1** double calcul = java.lang.Math.cos(54879854);
+        //double calcul = java.lang.Math.cos(54879854);
         if(numero <= 1) return numero;
         UF2ACT3 fib1 = new UF2ACT3(numero-1);
         //fib1.fork();
@@ -18,8 +18,18 @@ public class UF2ACT3 extends RecursiveTask<Long> {
 	 return fib1.compute()+ fib2.join();
 	 }
     
+    public static long calculaFibonacci(long numero) {
+        //double calcul = java.lang.Math.cos(54879854);
+        if (numero==0) { return 0; }
+        else if (numero==1) { return 1; }
+        else { 
+            return (calculaFibonacci(numero-2) + calculaFibonacci(numero-1)); 
+        }
+    }
+
     public static void main(String[] args){
         ForkJoinPool pool = new ForkJoinPool();
-        System.out.println("Calculat:  " + pool.invoke(new UF2ACT3(40)));    
+        System.out.println("Calculat:  " + pool.invoke(new UF2ACT3(46)));
+        System.out.println("Calculat:  " + calculaFibonacci(46));
     }
 }
